8cc7367e26709ea79a44636769f6533315728e1b
Dan Gilbert <ddg@daentech.co.uk>
2023-06-08 10:16:19 +0200

    Downgrade version for pre-release

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? "AyeAye (Dev)" : "AyeAye",
     slug: "ayeaye",
-    version: "1.0.0",
+    version: "0.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
