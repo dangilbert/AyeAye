e6ac459604c988faf9a1baa0918e47f0aa73f227
Dan Gilbert <ddg@daentech.co.uk>
2023-06-08 10:13:33 +0200

    Set up app for installing both dev and another build

---
+++ app.config.js
@@ -0,0 +1,46 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+
+export default {
+  expo: {
+    name: IS_DEV ? "AyeAye (Dev)" : "AyeAye",
+    slug: "ayeaye",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: IS_DEV
+        ? "eu.dangilbert.ayeaye.dev"
+        : "eu.dangilbert.ayeaye",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: IS_DEV ? "eu.dangilbert.ayeaye.dev" : "eu.dangilbert.ayeaye",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "5abd0b3c-4752-4ae7-bf66-bfa58530a529",
+      },
+    },
+    owner: "perketel",
+    runtimeVersion: {
+      policy: "sdkVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/5abd0b3c-4752-4ae7-bf66-bfa58530a529",
+    },
+  },
+};
