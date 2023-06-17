f1e993b86b2c7d6a3e53686d7f2a123254aa4b8e
Dan Gilbert <ddg@daentech.co.uk>
2023-06-17 17:29:35 +0200

    Fix white flash when loading fullscreen image

--- src/components/media/MediaModalScreen.tsx
+++ src/components/media/MediaModalScreen.tsx
@@ -2,13 +2,23 @@ import { ImageZoom } from "@likashefqet/react-native-image-zoom";
 import { Theme, useTheme } from "@rn-app/theme";
 import { StyleSheet, View } from "react-native";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+import { ActivityIndicator } from "react-native-paper";
 
 export const MediaModalScreen = gestureHandlerRootHOC(({ route }) => {
   const imageUri = route.params?.imageUri;
   const themedStyle = styles(useTheme());
   return (
     <View style={themedStyle.container}>
-      {imageUri && <ImageZoom uri={imageUri} />}
+      {imageUri && (
+        <ImageZoom
+          uri={imageUri}
+          renderLoader={() => (
+            <View style={{ flex: 1 }}>
+              <ActivityIndicator />
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 });
